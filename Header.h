9310398be8def4903e1a9b85c29bccc391a56839
OriginalMidas <clayton.brez@gmail.com>
2015-03-13 02:10:19 -0600

    added more sorting alogorithms and furthur worked on the UI

---
+++ Header.h
@@ -0,0 +1,27 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+using namespace std;
+
+const int MAX_ARRAY_SIZE = 20;
+
+/* Other Functions */
+void nullArray(int generateArray[]);
+void generateRandomNumbers(int generateArray[], int arraySize);
+void printOut(int numberArray[MAX_ARRAY_SIZE]);
+void copyArray(int primaryArray[], int copiedArray[]);
+
+/* Sorting Functions */
+void insertionSort(int arr[], int length);
+void mergeSort(int a[], int low, int high);
+void quickSort(int a[], int first, int last);
+int pivot(int a[], int first, int last);
+void swap(int& a, int& b);
+void swapNoTemp(int& a, int& b);
+void print(int a[], const int& N);
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,50 @@
+#ifndef MAIN_CPP
+#define MAIN_CPP
+
+#include "Header.h"
+
+void printOut(int numberArray[MAX_ARRAY_SIZE]);
+int main()
+{
+	int primaryNumberArray[MAX_ARRAY_SIZE];
+	int tempSortArray[MAX_ARRAY_SIZE];
+	int arraySize = -5;
+	int k;
+
+	while (arraySize >= MAX_ARRAY_SIZE || arraySize <= 0)
+	{
+		cout << "The max amount of numbers you can use is " << MAX_ARRAY_SIZE << " Please enter how many numbers you will be using:" << endl;
+		cin >> arraySize;
+	}
+	cout << "Please enter the k value for quick-insertion" << endl;
+	cin >> k;
+
+	srand(time(NULL)); // generate random numbers
+
+	// Create the array of random numbers and print the array out to show that it is randomn
+	generateRandomNumbers(primaryNumberArray, arraySize);
+	printOut(primaryNumberArray);
+
+	// make the tempSortArray the same as the primaryNumberArray, then sort the temp array using insertion sort
+	copyArray(primaryNumberArray, tempSortArray);
+	insertionSort(tempSortArray, arraySize);
+
+	// reset the tempSortArray and use quick sort
+	copyArray(primaryNumberArray, tempSortArray);
+	cout << "Quick Sort -- " << endl;
+	quickSort(tempSortArray, 0, arraySize); 
+	printOut(tempSortArray);
+
+	//
+	copyArray(primaryNumberArray, tempSortArray);
+	mergeSort(tempSortArray, 0, arraySize);
+
+	//
+	//copyArray(primaryNumberArray, tempSortArray);
+	//QuickInsertion();
+	nullArray(primaryNumberArray);
+	char temp;
+	cout << endl << "please press any key to continue ... " << endl;
+	cin >> temp;
+}
+#endif
